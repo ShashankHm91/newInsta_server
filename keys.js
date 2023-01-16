0383be44b307ed184ea6b2c9dd9e3a63e2fb787e
SHASHANK H M <113587615+ShashankHm91@users.noreply.github.com>
2023-01-16 22:45:48 +0530

    server updated

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://shashankHM:shashankhm123@cluster09.jmmm0pw.mongodb.net/My_insta?retryWrites=true&w=majority",
+    Jwt_secret: "SHASHANK_HM__INSTACLONE"
+
+};
